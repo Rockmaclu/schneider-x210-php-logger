30271a8cafe1f4c4a5b62f84f8ecfe36f6311f06
Manuel Chichi <manuchichi@hotmail.com>
2018-11-17 00:17:06 -0300

    Now it sends the data correctly

--- send_lecture.php
+++ send_lecture.php
@@ -2,20 +2,33 @@
 <?php
     require 'connection.php';
     $tmpName = $_FILES['csv']['tmp_name'];
-    $csvAsArray = array_map('str_getcsv', file($tmpName));
+
+    $csvAsArray = array_map( function($d) {return str_getcsv($d, "\t"); }, file($tmpName));
 
     $lectures = array_slice($csvAsArray,7);
 
     foreach ($lectures as $value) {
       $error = $value[0];
       $utcoffset = $value[1];
-      $localTimeStamp = $value[3];
-      $aparentEnergy = $value[4];
-      $realEnergy = $value[5];
-      $reactiveEnergy = $value[6];
+      $lecturetimestamp = $value[2];
+      $frecuency = $value[3];
+      $currentA = $value[4];
+      $currentB = $value[5];
+      $currentC = $value[6];
+      $currentM = $value[7];
+      $voltageAB = $value[8];
+      $voltageBC = $value[9];
+      $voltageCA = $value[10];
+      $powerFactor = $value[11];
+      $activepower = $value[12];
+      $reactivepower = $value[13];
+      $activeenergydelivered = $value[14];
+      $reactiveenergydelivered = $value[15];
+      $demandactivepower = $value[16];
+      $demandreactivepower = $value[17];
 
-      $sql = "INSERT INTO lectures (error, utcoffset,lecturetimestamp,aparentenergy,realenergy,reactiveenergy)
-      VALUES ($error, $utcoffset,$localTimeStamp,$aparentEnergy,$realEnergy,$reactiveEnergy)";
+      $sql = "INSERT INTO lectures (error,utcoffset,lecturetimestamp,frecuency,currentA,currentB,currentC,currentN,voltageAB,voltageBC,voltageCA,powerFactor,activepower,reactivepower,activeenergydelivered,reactiveenergydelivered,demandactivepower,demandreactivepower)
+      VALUES ($error, $utcoffset,'$lecturetimestamp',0,$currentA,$currentB,$currentC,$currentM,$voltageAB,$voltageBC,$voltageCA,0,$activepower,$reactivepower,$activeenergydelivered,$reactiveenergydelivered,$demandactivepower,$demandreactivepower)";
 
       if (mysqli_query($conn, $sql)) {
           echo "New record created successfully <br>";
